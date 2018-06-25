ec4dbe108811986bbc2cabfa1bfd73d4d865979e
Jason Dillon <jdillon@users.noreply.github.com>
2018-06-25 15:11:19 -0700

    remove maven-extension

--- maven-extension/src/it/projects/eventspy/verify.groovy
+++
@@ -1,3 +0,0 @@
-File buildLog = new File(basedir, 'build.log')
-// FIXME: adjust assertion
-//assert buildLog.text.contains('org.sonatype.ossindex.maven.enforcer.BanVulnerableDependencies failed with message')
