deaa86dc2cbda85c328db4532564877d4b52cb5d
Jason Dillon <jdillon@users.noreply.github.com>
2018-06-25 14:05:47 -0700

    normalize maven-extension its; pending a lot more work here though; may disable

---
+++ maven-extension/src/it/projects/eventspy/verify.groovy
@@ -0,0 +1,3 @@
+File buildLog = new File(basedir, 'build.log')
+// FIXME: adjust assertion
+//assert buildLog.text.contains('org.sonatype.ossindex.maven.enforcer.BanVulnerableDependencies failed with message')
