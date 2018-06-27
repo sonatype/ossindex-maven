07a386d8f1aa93540c18a47257982cfa43f690e6
Jason Dillon <jdillon@users.noreply.github.com>
2018-06-27 13:51:15 -0700

    Add some additional assertions to help ensure content, in a very lax way

--- testsuite/src/it/projects/maven-plugin/export-txt/verify.groovy
+++ testsuite/src/it/projects/maven-plugin/export-txt/verify.groovy
@@ -3,3 +3,7 @@ assert buildLog.text.contains('[WARNING] Detected 1 vulnerable components')
 
 File report = new File(basedir, 'target/report.txt')
 assert report.exists()
+
+String text = report.text
+assert text.contains('Detected 1 vulnerable components:')
+assert text.contains('commons-fileupload:commons-fileupload:jar:1.3:compile;')
--- testsuite/src/it/projects/maven-plugin/export-xml/verify.groovy
+++ testsuite/src/it/projects/maven-plugin/export-xml/verify.groovy
@@ -3,3 +3,7 @@ assert buildLog.text.contains('[WARNING] Detected 1 vulnerable components')
 
 File report = new File(basedir, 'target/report.xml')
 assert report.exists()
+
+String text = report.text
+assert text.contains('<key>commons-fileupload:commons-fileupload:jar:1.3:compile</key>')
+assert text.contains('<coordinates>maven:commons-fileupload/commons-fileupload@1.3</coordinates>')
