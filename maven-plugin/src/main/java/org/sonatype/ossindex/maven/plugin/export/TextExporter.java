3c422e01203dc5b083a43d7f4e1fa1a3730e62b4
Jason Dillon <jdillon@users.noreply.github.com>
2018-06-26 19:51:37 -0700

    Add Text exporter stub

---
+++ maven-plugin/src/main/java/org/sonatype/ossindex/maven/plugin/export/TextExporter.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2018-present Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.ossindex.maven.plugin.export;
+
+import java.io.Writer;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.sonatype.ossindex.maven.common.ComponentReportResult;
+import org.sonatype.ossindex.service.client.internal.GsonMarshaller;
+
+/**
+ * ???
+ *
+ * @since ???
+ */
+@Named
+@Singleton
+public class TextExporter
+  extends ExporterSupport
+{
+  @Override
+  protected boolean accept(final String filename) {
+    return filename.endsWith(".txt");
+  }
+
+  @Override
+  protected void export(final ComponentReportResult result, final Writer writer) throws Exception {
+    // TODO:
+  }
+}
