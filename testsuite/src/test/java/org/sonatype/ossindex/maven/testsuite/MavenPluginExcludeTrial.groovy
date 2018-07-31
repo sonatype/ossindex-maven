15b3a8d6079304426569dbef851bb8e4265bbabb
Jason Dillon <jdillon@users.noreply.github.com>
2018-07-31 11:15:07 -0700

    add trial helper to test excludes manually

---
+++ testsuite/src/test/java/org/sonatype/ossindex/maven/testsuite/MavenPluginExcludeTrial.groovy
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2018-present Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.ossindex.maven.testsuite
+
+/**
+ * {@code maven-plugin} exclude trials.
+ */
+class MavenPluginExcludeTrial
+    extends TestsuiteSupport
+{
+  MavenPluginExcludeTrial() {
+    super('3.5.4')
+    arguments = [
+        '-Dinvoker.test=maven-plugin/exclude-*'
+    ]
+    overrides = [
+        // send requests to dev instance
+        'ossindex.baseUrl': 'http://10.20.5.63:8080/'
+    ]
+  }
+}
